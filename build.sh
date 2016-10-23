c23720bd57ec3f1a6f3bef7738d0b7a258d28643
Diego Cortassa <diego@cortassa.net>
2016-10-23 12:46:08 +0200

    Updated to Tatctic 4.5 and renamed docker.io to docker for ubuntu 16.04

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 TIMESTAMP=`date +%Y-%m-%d`
-alias docker='sudo docker.io'
+alias docker='sudo docker'
 sed -i "s/ENV REFRESHED_AT .*$/ENV REFRESHED_AT $TIMESTAMP/" Dockerfile
 docker build -t diegocortassa/tactic .
-docker build -t diegocortassa/tactic:4.4.0.v03 .
+docker build -t diegocortassa/tactic:4.5.0.v01 .
 docker push diegocortassa/tactic
