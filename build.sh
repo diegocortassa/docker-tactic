014377b049052ade2633c287218c27591ac21bb3
Diego Cortassa <diego@cortassa.net>
2021-03-29 23:14:05 +0200

    Update to 4.8.0

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ alias docker='sudo docker'
 sed -i "s/ENV REFRESHED_AT .*$/ENV REFRESHED_AT $TIMESTAMP/" Dockerfile
 docker build -t diegocortassa/tactic .
 docker build -t diegocortassa/tactic:4.8 .
-docker build -t diegocortassa/tactic:4.8.0.b05 .
+docker build -t diegocortassa/tactic:4.8.0 .
 docker push diegocortassa/tactic
