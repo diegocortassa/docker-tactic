2ec06ab3811cc59a557495950d1c9e5b4f515719
Diego Cortassa <diego@cortassa.net>
2021-09-27 15:26:04 +0200

    Push version tags

--- build.sh
+++ build.sh
@@ -5,3 +5,5 @@ sudo docker build -t diegocortassa/tactic .
 sudo docker build -t diegocortassa/tactic:4.8 .
 sudo docker build -t diegocortassa/tactic:4.8.0 .
 sudo docker push diegocortassa/tactic
+sudo docker push diegocortassa/tactic:4.8
+sudo docker push diegocortassa/tactic:4.8.0
